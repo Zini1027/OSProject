10a6f4aa485b6b0726b2dc79c22286c6f101cc4b
ahsyuyu <ahsyuyu@gmail.com>
2015-11-28 18:51:20 -0800

    reduce the search region to only uncompressed section in findVictim

--- Eclipse/nachos/machine/Processor.java
+++ Eclipse/nachos/machine/Processor.java
@@ -306,25 +306,25 @@ public final class Processor {
         ArrayList<Integer> victims = new ArrayList<Integer>();
 
         int i = 0;
-        while (victims.size() < victimNum && i < translations.length) {
+        while (victims.size() < victimNum && i < numPhysPages) {
             if (translations[i].valid && !translations[i].used && !translations[i].dirty)
                 victims.add(i);
             i++;
         }
         i = 0;
-        while (victims.size() < victimNum && i < translations.length) {
+        while (victims.size() < victimNum && i < numPhysPages) {
             if (translations[i].valid && !translations[i].used && translations[i].dirty)
                 victims.add(i);
             i++;
         }
         i = 0;
-        while (victims.size() < victimNum && i < translations.length) {
+        while (victims.size() < victimNum && i < numPhysPages) {
             if (translations[i].valid && translations[i].used && !translations[i].dirty)
                 victims.add(i);
             i++;
         }
         i = 0;
-        while (victims.size() < victimNum && i < translations.length) {
+        while (victims.size() < victimNum && i < numPhysPages) {
             if (translations[i].valid && translations[i].used && translations[i].dirty)
                 victims.add(i);
             i++;
